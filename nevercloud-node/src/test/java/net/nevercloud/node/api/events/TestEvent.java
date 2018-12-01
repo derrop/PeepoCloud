f9778eea3ad485ec8572fab2046630bc8cb778e4
McRuben <roclaromi@gmail.com>
2018-12-01 14:19:07 +0100

    Removed EventManager tests

--- nevercloud-node/src/test/java/net/nevercloud/node/api/events/TestEvent.java
+++
@@ -1,9 +0,0 @@
-package net.nevercloud.node.api.events;
-/*
- * Created by Mc_Ruben on 28.11.2018
- */
-
-import net.nevercloud.node.api.event.internal.Event;
-
-public class TestEvent extends Event {
-}
