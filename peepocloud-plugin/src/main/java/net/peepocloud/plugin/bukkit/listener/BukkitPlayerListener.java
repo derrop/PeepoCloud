4aaa7cc6cec6801d9fa1461ad93f7e30ee35893d
McRuben <roclaromi@gmail.com>
2019-01-04 18:14:34 +0100

    Cached players that are on the current bungee/bukkit online

--- peepocloud-plugin/src/main/java/net/peepocloud/plugin/bukkit/listener/BukkitPlayerListener.java
+++ peepocloud-plugin/src/main/java/net/peepocloud/plugin/bukkit/listener/BukkitPlayerListener.java
@@ -2,10 +2,12 @@ package net.peepocloud.plugin.bukkit.listener;
 
 
 import net.peepocloud.lib.player.PeepoPlayer;
+import net.peepocloud.plugin.PeepoCloudPlugin;
 import net.peepocloud.plugin.bukkit.PeepoBukkitPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class BukkitPlayerListener implements Listener {
 
@@ -20,10 +22,16 @@ public class BukkitPlayerListener implements Listener {
         PeepoPlayer peepoPlayer = this.bukkitAPI.getPlayer(event.getUniqueId()).complete();
         if(peepoPlayer != null) {
             // TODO check if player tries to join the server from bungeeCord
-            // TODO cache Player
+
+            PeepoCloudPlugin.getInstance().getCachedPlayers().put(peepoPlayer.getUniqueId(), peepoPlayer);
         } else {
             event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "§cYou have to connect to a bungeecord-server first!"); // TODO: configurable
         }
+    }
+
+    @EventHandler
+    public void handleQuit(PlayerQuitEvent event) {
+        PeepoCloudPlugin.getInstance().getCachedPlayers().remove(event.getPlayer().getUniqueId());
     }
 
 
