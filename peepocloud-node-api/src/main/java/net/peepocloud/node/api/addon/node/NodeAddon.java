9ae32caca330a662a2d0c540b95bab579c4e85ec
McRuben <roclaromi@gmail.com>
2019-01-05 20:11:51 +0100

    Added initPacketHandlers method to the NodeAddon

--- peepocloud-node-api/src/main/java/net/peepocloud/node/api/addon/node/NodeAddon.java
+++ peepocloud-node-api/src/main/java/net/peepocloud/node/api/addon/node/NodeAddon.java
@@ -3,6 +3,7 @@ package net.peepocloud.node.api.addon.node;
  * Created by Mc_Ruben on 05.11.2018
  */
 
+import net.peepocloud.lib.network.packet.PacketManager;
 import net.peepocloud.node.api.addon.Addon;
 
 public class NodeAddon extends Addon {
@@ -17,4 +18,6 @@ public class NodeAddon extends Addon {
     @Override
     public void onDisable() {
     }
+
+    public void initPacketHandlers(PacketManager packetManager) {}
 }
